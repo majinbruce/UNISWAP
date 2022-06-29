c0650ca5ebbbdf4236c7d06327420b8e9c48187e
omkar <omkar.c.344@gmail.com>
2022-06-29 12:53:20 +0530

    configured rinkeby network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,39 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-require("@nomiclabs/hardhat-waffle");
-require("dotenv").config({ path: "../.env" });
-require("@nomiclabs/hardhat-etherscan");
+const {
+  RINKEBY_PRIVATE_KEY,
+  ALCHEMY_API_KEY,
+  ETHERSCAN_API_KEY,
+  RINKEBY_PRIVATE_KEY_2,
+} = process.env;
 
-const { RINKEBY_PRIVATE_KEY, ALCHEMY_API_KEY, ETHERSCAN_API_KEY } = process.env;
 module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
       url: ALCHEMY_API_KEY,
-      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`, `0x${RINKEBY_PRIVATE_KEY_2}`],
+      gasPrice: 20e9,
+      gas: 25e6,
     },
 
     hardhat: {
